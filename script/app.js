e9eae5849435ea5ca9aea9c5e93c0d111839e7bc
Marcos Willian <166168504+MarcosWillianA@users.noreply.github.com>
2024-10-10 11:22:07 -0400

    Fiz um pouco
    salvando pra garantir

--- script/app.js
+++ script/app.js
@@ -4,27 +4,37 @@ const pergunta = document.querySelector('#pergunta');
 const respostas = document.querySelectorAll('.respostas');
 const proxima = document.querySelector('#proxima');
 
+let perguntaAtual = 0;
+
 class Quiz {
-    constructor (pergunta, opcoes, correta) {
-        this.pergunta = pergunta;
+    constructor (questao, opcoes, correta) {
+        this.questao = questao;
         this.opcoes = opcoes;
         this.correta = correta;
     };
 
     mostrarPergunta() {
-        const perguntaQuiz = this.perguntas[this]
+        const perguntaQuiz = this.perguntas[perguntaAtual];
+        pergunta.innerHTML = perguntaQuiz.questao;
+        respostas.forEach((opcao, index) => {
+            opcao = perguntaQuiz.opcoes[index];
+            respostas.innerHTML = opcao;
+        })
     }
 };
 
 const perguntas = [
     {
-        pergunta: 'Qual é o maior mamífero do mundo?',
+        questao: 'Qual é o maior mamífero do mundo?',
         opcoes: ['elefante', 'baleia azul', 'girafa', 'orca'],
         correta: 1
     },
     {
-        pergunta: 'Qual é a capital da França?',
+        questao: 'Qual é a capital da França?',
         opcoes: ['roma', 'paris', 'madri', 'berlim'],
         correta: 1
     }
 ];
+
+const meuQuiz = new Quiz(perguntas);
+meuQuiz.mostrarPergunta(); // Mostra a primeira pergunta
